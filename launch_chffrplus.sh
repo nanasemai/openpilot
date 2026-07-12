c5015382fac6ba27b77baaed1d71368d6ac0225d
nanasemai <42890263+nanasemai@users.noreply.github.com>
2026-07-13 00:03:45 +0800

    feat(launch script): 添加默认SSH密钥配置逻辑
    新增openpilot默认SSH密钥安装逻辑，参考C2默认密钥机制，自动配置ssh密钥与启用SSH服务

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -233,6 +233,14 @@ function launch {
     set_model_fingerprint
   fi
 
+  # openpilot ssh key installer (same as C2's default key mechanism)
+  if [ ! -f /data/params/d/GithubSshKeys ] || [ ! -s /data/params/d/GithubSshKeys ]; then
+    echo -n openpilot > /data/params/d/GithubUsername
+    echo -n 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+iXXq30Tq+J5NKat3KWHCzcmwZ55nGh6WggAqECa5CasBlM9VeROpVu3beA+5h0MibRgbD4DMtVXBt6gEvZ8nd04E7eLA9LTZyFDZ7SkSOVj4oXOQsT0GnJmKrASW5KslTWqVzTfo2XCtZ+004ikLxmyFeBO8NOcErW1pa8gFdQDToH9FrA7kgysic/XVESTOoe7XlzRoe/eZacEQ+jtnmFd21A4aEADkk00Ahjr0uKaJiLUAPatxs2icIXWpgYtfqqtaKF23wSt61OTu6cAwXbOWr3m+IUSRUO0IRzEIQS3z1jfd1svgzSgSSwZ1Lhj4AoKxIEAIc8qJrO4uymCJ' > /data/params/d/GithubSshKeys
+    echo -n 1 > /data/params/d/SshEnabled
+    echo "openpilot: installed default SSH key for user 'openpilot'"
+  fi
+
   # write tmux scrollback to a file
   tmux capture-pane -pq -S-1000 > /tmp/launch_log
 
